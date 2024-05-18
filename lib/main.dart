3e13313a1b5dcb80beebb9ef3d06d40cd030b485
jgbor <100523032+jgbor@users.noreply.github.com>
2024-05-18 13:05:20 +0200

    random color swap demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -44,11 +46,104 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
+      home: MyHomePage(key: UniqueKey(), title: 'Flutter Demo'),
       //DO NOT MODIFY
       navigatorObservers: GetIt.I<List<NavigatorObserver>>(),
       //DO NOT MODIFY
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title = ""}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Widget> _myColorWidgets = [
+    RandomColorWidget(key: UniqueKey()),
+    RandomColorWidget(key: UniqueKey())
+  ];
+
+  void _reverseList() {
+    setState(() {
+      var temp = _myColorWidgets[0];
+      _myColorWidgets[0] = _myColorWidgets[1];
+      _myColorWidgets[1] = temp;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'My colored widgets: ',
+            ),
+            ..._myColorWidgets
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _reverseList,
+        tooltip: 'Reverse',
+        child: Icon(Icons.swap_vert),
+      ),
+    );
+  }
+}
+
+class StatelessColorWidget extends StatelessWidget {
+  final Color color;
+
+  const StatelessColorWidget({Key? key, this.color = Colors.transparent})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120,
+      height: 40,
+      color: color,
+    );
+  }
+}
+
+class RandomColorWidget extends StatefulWidget {
+  const RandomColorWidget({Key? key}) : super(key: key);
+
+  @override
+  _RandomColorWidgetState createState() => _RandomColorWidgetState();
+}
+
+class _RandomColorWidgetState extends State<RandomColorWidget> {
+  late Color currentColor;
+
+  @override
+  void initState() {
+    var rnd = Random();
+    currentColor =
+        Color.fromRGBO(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256), 1);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StatelessColorWidget(
+      color: currentColor,
     );
   }
 }
