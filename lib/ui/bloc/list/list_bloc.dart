80d7da46775c6ad40dec76e93ff93d0d2fe9e094
jgbor <100523032+jgbor@users.noreply.github.com>
2024-05-19 19:12:09 +0200

    list bloc base

--- lib/ui/bloc/list/list_bloc.dart
+++ lib/ui/bloc/list/list_bloc.dart
@@ -3,11 +3,25 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_homework/network/user_item.dart';
 import 'package:meta/meta.dart';
 
+import '../../../network/remote_service.dart';
+
 part 'list_event.dart';
 part 'list_state.dart';
 
 class ListBloc extends Bloc<ListEvent, ListState> {
   ListBloc() : super(ListInitial()) {
-
+    on<ListLoadEvent>(
+        (event, emit) async {
+          if (state is ListLoading) return;
+          emit(ListLoading());
+          try {
+            var users = await remoteService.getUsers();
+            emit(ListLoaded(users));
+          } catch (e) {
+            emit(ListError(e.toString()));
+            emit(ListInitial());
+          }
+        }
+    );
   }
 }
