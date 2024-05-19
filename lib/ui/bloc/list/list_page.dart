985a749ff6beea69832dba0ad90290ba5c4bdf3f
jgbor <100523032+jgbor@users.noreply.github.com>
2024-05-19 21:13:57 +0200

    logout

--- lib/ui/bloc/list/list_page.dart
+++ lib/ui/bloc/list/list_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_homework/ui/bloc/list/list_bloc.dart';
@@ -13,14 +12,59 @@ class ListPageBloc extends StatefulWidget {
 }
 
 class _ListPageBlocState extends State<ListPageBloc> {
-
   @override
   void initState() {
+    context.read<ListBloc>().add(ListLoadEvent());
     super.initState();
+  }
+
+  void _logout() {
+    GetIt.I<SharedPreferences>().remove('token');
+    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
   }
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError('Page not implemented!');
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Users'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: _logout,
+            )
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: BlocConsumer<ListBloc, ListState>(
+            listenWhen: (_, state) => state is ListError,
+            listener: (context, state) {
+              if (state is ListError) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(state.message)));
+              }
+            },
+            buildWhen: (_, state) => state is! ListError,
+            builder: (context, state) {
+              if (state is ListLoading) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is ListLoaded) {
+                return ListView.builder(
+                  itemCount: state.users.length,
+                  itemBuilder: (context, index) {
+                    final item = state.users[index];
+                    return ListTile(
+                      title: Text(item.name),
+                    );
+                  },
+                );
+              } else {
+                return const Center(child: Text('Unknown state'));
+              }
+            },
+          ),
+        )
+    );
   }
 }
